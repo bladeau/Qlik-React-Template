a844bd494fe6ed0e0f71da230c0e9debb806c53d
Al Almaktoum <al.almaktoum@live.com>
2023-02-03 16:05:56 +1100

    Rendering with missing parts

--- src/components/SomeConsumer.jsx
+++ src/components/SomeConsumer.jsx
@@ -2,13 +2,25 @@ import React, { useContext } from "react";
 import { QDocContext } from "./QDoc";
 
 const SomeConsumer = () => {
+  const chartElement = React.useRef(null);
+
   const { nebula } = useContext(QDocContext); //Retrieve Embed instance from Context... https://qlik.dev/apis/javascript/nebulajs-stardust/#%23%2Fdefinitions%2FEmbed
 
   setTimeout(() => {
-    console.log(nebula.getRegisteredTypes());
+    // console.log(nebula.render());
+
+    nebula.render({
+      element: chartElement.current,
+      id: "qZPdytp",
+    });
   }, 10000);
 
-  return <></>;
+  return (
+    <>
+      {" "}
+      <div ref={chartElement} />
+    </>
+  );
 };
 
 export default SomeConsumer;
