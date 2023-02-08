4cd3aae3c5dec1e6380514486e8e0750b3cf9db4
Al Almaktoum <al.almaktoum@live.com>
2023-02-08 11:22:14 +1100

    Loading Table Data but not rendering correct

--- src/components/TablePage.jsx
+++ src/components/TablePage.jsx
@@ -1,29 +1,56 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { QDocContext } from "./QDocProvider";
 
 const TablePage = () => {
   const enigma = useContext(QDocContext);
+  const [table, settable] = useState("Loading...");
   //Setup DOM References
 
   const initEnigmaAppObject = async () => {
     //https://qlik.dev/libraries-and-tools/enigmajs
-    enigma.getObject("WTZDvr").then((api) => {
-      // api is now an object with QIX interface methods for the GenericObject struct
-      api.getLayout().then(() => {
-        // 'getProperties' will give you the underlying properties describing
-        // the generic object layout, including the hypercube/expression sources etc.:
-        api.getProperties().then((props) => {
-          // modify some properties to you liking:
-          props.someValue = true;
-          console.log(props);
-          // 'setProperties' will modify the generic object state from 'valid' to
-          // 'invalid' in Qlik Associative Engine, causing a 'changed' event in enigma.js:
-          api.setProperties(props).then(() => {
-            // the 'changed' event handler above should now have been invoked
-          });
-        });
-      });
-    });
+
+    const model = await enigma.getObject("WTZDvr"); // It's a table
+    const modelLayout = await model.getLayout();
+    const data = [];
+    const columns = modelLayout.qHyperCube.qSize.qcx;
+    const totalheight = modelLayout.qHyperCube.qSize.qcy;
+
+    // Replace pageheight & numberOfPages number with the comments to get all data
+    const pageheight = Math.floor(10000 / columns);
+    const numberOfPages = Math.ceil(totalheight / pageheight);
+
+    for (let i = 0; i < numberOfPages; i++) {
+      const page = {
+        qTop: pageheight * i,
+        qLeft: 0,
+        qWidth: columns,
+        qHeight: pageheight,
+      };
+      const row = await model.getHyperCubeData("/qHyperCubeDef", [page]);
+      data.push(...row[0].qMatrix);
+    }
+
+    const _table = data.map((x) => `<tr><td>${x[0].qText}</td><td>${x[1].qText}</td><td>${x[2].qText}</td></tr>`);
+
+    settable(_table);
+
+    // enigma.getObject("WTZDvr").then((api) => {
+    //   // api is now an object with QIX interface methods for the GenericObject struct
+    //   api.getLayout().then(() => {
+    //     // 'getProperties' will give you the underlying properties describing
+    //     // the generic object layout, including the hypercube/expression sources etc.:
+    //     api.getProperties().then((props) => {
+    //       // modify some properties to you liking:
+    //       props.someValue = true;
+    //       console.log(props);
+    //       // 'setProperties' will modify the generic object state from 'valid' to
+    //       // 'invalid' in Qlik Associative Engine, causing a 'changed' event in enigma.js:
+    //       api.setProperties(props).then(() => {
+    //         // the 'changed' event handler above should now have been invoked
+    //       });
+    //     });
+    //   });
+    // });
   };
 
   useEffect(() => {
@@ -32,7 +59,7 @@ const TablePage = () => {
 
   return (
     <>
-      <div>will output jsx table</div>
+      <div> {table}</div>
     </>
   );
 };
