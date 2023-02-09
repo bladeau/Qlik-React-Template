b617900b3ae8beb2fd373bb0724a8f85256c507f
Al Almaktoum <al.almaktoum@live.com>
2023-02-10 09:45:51 +1100

    rerieving a variable

--- src/components/VariablesPage.jsx
+++ src/components/VariablesPage.jsx
@@ -3,50 +3,15 @@ import { QDocContext } from "./QDocProvider";
 
 const VariablesPage = () => {
   const enigma = useContext(QDocContext);
-  const [tableHeaders, settableHeaders] = useState(null);
-  const [tableData, settableData] = useState(null);
+
   //Setup DOM References
 
   const initEnigmaAppObject = async () => {
     //https://qlik.dev/libraries-and-tools/enigmajs
 
     // Get Table and Data
-    const model = await enigma.getObject("WTZDvr"); // It's a table
-    const modelLayout = await model.getLayout();
-    const data = [];
-    const columns = modelLayout.qHyperCube.qSize.qcx;
-    const totalheight = modelLayout.qHyperCube.qSize.qcy;
-
-    // Replace pageheight & numberOfPages number with the comments to get all data
-    const pageheight = Math.floor(10000 / columns);
-    const numberOfPages = Math.ceil(totalheight / pageheight);
-
-    for (let i = 0; i < numberOfPages; i++) {
-      const page = {
-        qTop: pageheight * i,
-        qLeft: 0,
-        qWidth: columns,
-        qHeight: pageheight,
-      };
-      const row = await model.getHyperCubeData("/qHyperCubeDef", [page]);
-      data.push(...row[0].qMatrix);
-    }
-
-    settableData(data);
-
-    // Get Table Headers
-    const dimensionInfo = modelLayout.qHyperCube.qDimensionInfo.map((item) => {
-      return {
-        title: item.qFallbackTitle,
-      };
-    });
-    const measureInfo = modelLayout.qHyperCube.qMeasureInfo.map((item) => {
-      return {
-        title: item.qFallbackTitle,
-      };
-    });
-
-    settableHeaders(dimensionInfo.concat(measureInfo));
+    const aVariable = await enigma.getVariableByName("vMyVariable"); // It's a table
+    console.log(aVariable);
   };
 
   useEffect(() => {
